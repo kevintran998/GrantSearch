56fbf584bb526d14f2bc524911e78f69385e1d7d
Kevin Tran <kevintran998@gmail.com>
2020-06-20 22:50:24 -0500

    Update MainActivity.java

--- app/src/main/java/com/kkt160130/grantsearch/MainActivity.java
+++ app/src/main/java/com/kkt160130/grantsearch/MainActivity.java
@@ -9,7 +9,6 @@
 * the info into objects. The grant objects are held within an array list which is
 * then filtered based on the user's initial inputs. The app displays the filtered list onto the
 * screen when finished. The user is given the option to scroll through the list and click on each
-* grant for more information.s
 * grant for more information.
  */
 package com.kkt160130.grantsearch;
